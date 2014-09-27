7a5f49d33fab9037dee0835923729dad30159755
ilovepjs <hpsjoshi@gmail.com>
2014-09-27 21:46:23 +0100

    Handled autocomplete by country and sending data server side.

--- porn/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class CountryForm(forms.Form):
-	country = forms.CharField( widget=forms.TextInput(attrs={'placeholder': 'E.G United States'}), max_length=500)
--- porn/views.py
+++ porn/views.py
@@ -1,20 +1,21 @@
-from django.core import serializers
-from django.shortcuts import render
-from porn.models import Place
-from forms import CountryForm
-from countries import countries
 import json
+
+from django.core import serializers
+from django.shortcuts import render, render_to_response, redirect, RequestContext
+from django.views.decorators.http import require_POST
+
+from countries import countries
+from porn.models import Place
 
 def index(request):
 	context = {
 		'places': serializers.serialize('json', Place.objects.all()),
 		'countries' : countries.values(),
-        'form': CountryForm(),
 	}
 	return render(request, 'porn/index.html', context)
 
+@require_POST
 def country(request):
-	context = {
-		'places': serializers.serialize('json', Place.objects.all()),
-	}
-	return render(request, 'porn/index.html', context)
+	country = request.POST.get('country')
+	# open carosoul and show images
+	return redirect('/')
