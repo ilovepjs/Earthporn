c05405b1d20bb2ae9f33f6398840eb9d2cdeed78
Preeya Joshi <hpsjoshi@gmail.com>
2014-09-30 14:59:30 +0100

    Updated for Heroku.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn earthporn.wsgi:application --log-file=-
--- earthporn/settings.py
+++ earthporn/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'earthporn',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -86,3 +87,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
